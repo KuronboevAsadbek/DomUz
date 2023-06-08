993f7f042a85c74881ed1090dae64f421076404d
KuronboevAsadbek <asadbekiller@gmail.com>
2023-06-08 23:35:01 +0500

    added swagger2

---
+++ src/main/java/uz/DomUz/config/SpringFoxConfig.java
@@ -0,0 +1,31 @@
+package uz.DomUz.config;
+
+import io.swagger.annotations.Api;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+@EnableWebMvc
+public class SpringFoxConfig {
+
+    @Bean
+    public Docket docket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .enable(true)
+                .apiInfo(new ApiInfoBuilder()
+                        .title("Swagger Super")
+                        .description("Swagger Description details")
+                        .version("1.0").build())
+                .select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
+                .paths(PathSelectors.any()).build();
+    }
+}
--- src/main/java/uz/DomUz/repository/FileStorageRepository.java
+++
@@ -1,7 +0,0 @@
-package uz.DomUz.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import uz.DomUz.model.FileStorageModel;
-
-public interface FileStorageRepository extends JpaRepository<FileStorageModel, Long> {
-}
